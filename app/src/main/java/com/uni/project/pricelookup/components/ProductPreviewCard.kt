465acce088d404c810e8dbe8d8d171ce8d208d25
Csaba Dezsi <csabeszq2001@gmail.com>
2023-10-07 22:10:29 +0200

    egy shitty grid layout, shitty contentel. De működik :D

--- app/src/main/java/com/uni/project/pricelookup/components/ProductPreviewCard.kt
+++ app/src/main/java/com/uni/project/pricelookup/components/ProductPreviewCard.kt
@@ -1,22 +1,25 @@
 package com.uni.project.pricelookup.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.*
+import androidx.compose.material3.CardDefaults.shape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -28,14 +31,21 @@ import com.uni.project.pricelookup.R
 @Composable
 fun ProductPreviewCard() {
     ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 1.dp)
+            .size(width = 150.dp, height = 200.dp)
+            .clickable {
+                // TODO : to be implemented
+            },
+
         elevation = CardDefaults.cardElevation(
-            defaultElevation = 30.dp
-        ),
-        modifier = Modifier.size(width = 150.dp, height = 100.dp)
+            defaultElevation = 6.dp
+        )
     ) {
         Text(
             text = "This will be a pic of a chocolate",
-            modifier = Modifier.padding(16.dp),
+            Modifier.padding(all = 10.dp),
             textAlign = TextAlign.Center,
         )
     }
